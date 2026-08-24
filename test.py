9dceaec753cb3039ee9715485f25d54a1b442b76
AsarlaRajkumar <asarlarajkumar16@gmail.com>
2026-08-24 13:30:42 +0530

    update the focker files

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
-# result = tavily_search("provide me few best hotels in hyderabad")
-# print(result)
+# res = tavily_search("Best hotels in India")
+# print(res)
 
-result = search_flights("Plan 7 days trip from INDIA to Singapore")
-print(result)
+
+# res = search_flights("Plan a 7 days Nepal trip from Bangladesh")
+# print(res)
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
